3a85b3fd6f7c1a483337c8186e5d9f41b631bab5
migueluf <miguelcontacto2015@gmail.com>
2023-08-17 17:50:31 -0300

    atualizacao

--- teste.py
+++ teste.py
@@ -8,7 +8,7 @@ req = requests.get(url)
 info = req.json()
 
 df = pd.DataFrame(info['dados'], columns=["ID", "uri", "nome", "siglaPartido", "uriPartido", "siglaUf", "urlFoto", "email"])
-db_connection = 'mysql+pymysql://root:007458@localhost:3306/deputados'
+db_connection = 'mysql+pymysql://root:password@localhost:3306/deputados'
 db_connection = create_engine(db_connection)
 df.to_sql(con=db_connection, name='detalhes', if_exists='append', index=False)
 #df.info()
